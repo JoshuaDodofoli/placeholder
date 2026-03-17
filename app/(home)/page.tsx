8684484ddc5e61245554d5be8979399b6f90f7c2
JoshuaDodofoli <dodofolijoshua@gmail.com>
2026-03-17 19:08:24 +0000

    Enhance UI: Update styles and add Google Sans font to improve typography

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,10 +4,10 @@ import Wrapper from "../components/Wrapper";
 export default function Home() {
   return (
     <>
-      <section className="min-h-[55vh] xl:min-h-[70vh] flex flex-col justify-between w-full bg-amber-500">
+      <section className="min-h-[55vh] xl:min-h-[80vh] flex flex-col justify-between w-full bg-brand">
         <div className="mt-4 md:top-8">
           <Wrapper>
-            <span className="text-4xl lg:text-5xl tracking-tight font-bold flex items-center justify-start w-full">
+            <span className="text-3xl font-space lg:text-5xl tracking-tight font-bold flex items-center justify-start w-full">
               Placeholder
             </span>
           </Wrapper>
@@ -15,7 +15,7 @@ export default function Home() {
 
         <Wrapper>
           <div className="flex items-end justify-between w-full pb-4">
-            <h1 className="text-4xl lg:text-5xl tracking-tight md:text-6xl lg:max-w-2xl font-medium">Filling the gaps for ideas still in progress</h1>
+            <h1 className="text-3xl lg:text-5xl tracking-tight md:text-6xl lg:max-w-2xl font-medium">Filling the gaps for ideas still in progress</h1>
             <div className="hidden lg:flex">
               <div className="flex items-center justify-between gap-3 bg-white p-2">
                 <div className="flex items-center mr-8 gap-3">
@@ -38,8 +38,23 @@ export default function Home() {
         </Wrapper>
       </section>
 
-      <section className="bg-red-400">
-        work section
+      <section className="w-full">
+        <div className="w-full flex items-center justify-center">
+          {/* card */}
+          <div className="w-sm bg-red-300">
+            <div className="aspect-square w-full relative bg-stone">
+            </div>
+            <div className="w-full">
+              <div className="size-10 bg-stone"></div>
+              <div className="">
+                <span>Project name</span>
+                <p>Project description</p>
+              </div>
+            </div>
+          </div>
+
+          
+        </div>
       </section>
     </>
   );
