30d1342956d6d5f9a2103b9171dad810d28c005b
JoshuaDodofoli <dodofolijoshua@gmail.com>
2026-03-18 18:32:47 +0000

    Added data set

--- app/utils/data.ts
+++ app/utils/data.ts
@@ -1,29 +1,74 @@
 export const links = [
-    {
-        name: "Home",
-        path: "#"
-    },
-    {
-        name: "Works",
-        path: "#works"
-    },
-    {
-        name: "About",
-        path: "#about"
-    },
-    {
-        name: "Services",
-        path: "#services"
-    },
-    {
-        name: "Pricing",
-        path: "#pricing"
-    },
-]
+  {
+    name: "Home",
+    path: "#",
+  },
+  {
+    name: "Works",
+    path: "#works",
+  },
+  {
+    name: "About",
+    path: "#about",
+  },
+  {
+    name: "Services",
+    path: "#services",
+  },
+  {
+    name: "Pricing",
+    path: "#pricing",
+  },
+];
 
 export const services = [
-    "Photography",
-    "Branding",
-    "Illustration",
-    "App design",
-]
+  "Photography",
+  "Branding",
+  "Illustration",
+  "App design",
+];
+
+export const projects = [
+  {
+    name: "Project 1",
+    slug: "project-1",
+    caption: "Project caption",
+    desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc.",
+    service: "Photography",
+    images: [
+      "/images/photography/img1.webp",
+      "/images/photography/img2.webp",
+      "/images/photography/img3.webp",
+      "/images/photography/img4.webp",
+      "/images/photography/img5.webp",
+    ],
+  },
+  {
+    name: "Project 2",
+    slug: "project-2",
+    caption: "Project caption",
+    desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc.",
+    service: "Illustration",
+    images: [
+      "/images/illustration/img1.webp",
+      "/images/illustration/img2.webp",
+      "/images/illustration/img3.webp",
+      "/images/illustration/img4.webp",
+      "/images/illustration/img5.webp",
+    ],
+  },
+  {
+    name: "Project 3",
+    slug: "project-3",
+    caption: "Project caption",
+    desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc.",
+    service: "Branding",
+    images: [
+      "/images/branding/img1.webp",
+      "/images/branding/img2.webp",
+      "/images/branding/img3.webp",
+      "/images/branding/img4.webp",
+      "/images/branding/img5.webp",
+    ],
+  },
+];
