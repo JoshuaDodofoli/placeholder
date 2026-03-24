7882be89fb0d6b98ebdc2b3bbaa3cd75173a60fb
JoshuaDodofoli <dodofolijoshua@gmail.com>
2026-03-24 21:37:21 +0000

    Updated services section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -14,18 +14,18 @@ export default function Home() {
 
       <section className="bg-black text-paper w-full py-4">
         <Wrapper>
-          <div className="flex flex-col justify-between h-[50vh] md:h-[60vh]">
+          <div className="flex flex-col justify-between min-h-[35vh] xl:min-h-[60vh]">
             <div className="flex items-center gap-3">
               <span className="text-2xl leading-none font-bold">•</span>
-              <span className="font-medium">About Placeholder</span>
+              <span className="font-medium text-sm">About Placeholder</span>
             </div>
             <div className="">
-              <p className="text-3xl lg:text-5xl lg:max-w-2xl">Placeholder space between thought and completion.</p>
+              <p className="text-3xl lg:text-4xl xl:text-5xl md:max-w-sm lg:max-w-2xl">Placeholder space between thought and completion.</p>
               {/* <p className="text-sm text-stone/55 font-mono uppercase lg:max-w-xl font-bold mt-4"> We use AI generated images (slop) to explore,test and iterate on ideas. To build without overthinking and evolve without permission. <br />Placeholder gives your unfinished work a home.</p> */}
             </div>
           </div>
 
-          <div className="w-full aspect-square lg:aspect-video bg-paper mt-8 mb-12">
+          <div className="w-full aspect-square bg-paper mt-8 mb-12">
 
           </div>
         </Wrapper>
--- app/(home)/sections/Hero.tsx
+++ app/(home)/sections/Hero.tsx
@@ -1,40 +1,26 @@
 import Wrapper from '@/app/components/Wrapper'
+import BookCallButton from '../(components)/BookCallButton'
 
 const Hero = () => {
   return (
-     <section className="min-h-[55vh] xl:min-h-[80vh] flex flex-col justify-between w-full bg-brand">
-        <div className="mt-4 md:top-8">
-          <Wrapper>
-            <span className="text-3xl font-space lg:text-5xl tracking-tight font-bold flex items-center justify-start w-full">
-              Placeholder
-            </span>
-          </Wrapper>
-        </div>
-
+    <section className="min-h-[50vh] xl:min-h-[80vh] flex flex-col justify-between w-full">
+      <div className="mt-4 md:top-8">
         <Wrapper>
-          <div className="flex items-end justify-between w-full pb-4">
-            <h1 className="text-3xl lg:text-5xl tracking-tight md:text-6xl lg:max-w-2xl font-medium">Filling the gaps for ideas still in progress</h1>
-            <div className="hidden lg:flex">
-              <div className="flex items-center justify-between gap-3 bg-white p-2">
-                <div className="flex items-center mr-8 gap-3">
-                  <div className="size-17 bg-pink-400"></div>
-                  <div className="flex flex-col">
-                    <span className="text-sm text-dark-grey">Project discovery</span>
-                    <p className="text-sm font-medium">Book a call</p>
-                  </div>
-                </div>
-                <div className="size-10 bg-paper flex items-center justify-center rounded-full">
-                  <svg width="20" height="12" viewBox="0 0 20 12" fill="none">
-                    <line x1="0" y1="6" x2="18" y2="6" stroke="black" strokeWidth="2" strokeLinecap="round" />
-                    <line x1="12" y1="1" x2="19" y2="6" stroke="black" strokeWidth="2" strokeLinecap="round" />
-                    <line x1="12" y1="11" x2="19" y2="6" stroke="black" strokeWidth="2" strokeLinecap="round" />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </div>
+          <span className="text-3xl font-space lg:text-5xl tracking-tight font-bold flex items-center justify-start w-full">
+            Placeholder
+          </span>
         </Wrapper>
-      </section>
+      </div>
+
+      <Wrapper>
+        <div className="flex items-end justify-between w-full pb-4">
+          <h1 className="text-3xl md:text-4xl xl:text-5xl tracking-tight md:max-w-lg xl:max-w-2xl font-medium">Filling the gaps for ideas still in progress</h1>
+          <div className="hidden md:flex">
+            <BookCallButton />
+          </div>
+        </div>
+      </Wrapper>
+    </section>
   )
 }
 
